81b493232830632c5bd2288b4d09b4b0503e66e9
riiamri <riiamri23@gmail.com>
2023-05-28 12:42:22 +0700

    update api

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,4 +1,4 @@
 // export const consumet = "https://consumet-api.herokuapp.com";
-// export const consumet = "https://api.consumet.org";
-export const consumet = "http://0.0.0.0:3000";
+export const consumet = "https://api.consumet.org";
+// export const consumet = "http://0.0.0.0:3000";
 export const consumetProxy = "https://cors.proxy.consumet.org";
