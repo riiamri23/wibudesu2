46dc863e82761ee1fcd09dab392f7b04ee4a5d64
riiamri <riiamri23@gmail.com>
2023-05-29 13:05:21 +0700

    update

--- src/pages/Anime/InfoNew/Index.js
+++ src/pages/Anime/InfoNew/Index.js
@@ -5,7 +5,7 @@ import Navbar from '../../../components/navbar/Index';
 import InfoAnime from '../../../components/InfoAnime';
 
 
-const ListEps = ({name,episodes})=>(<div className="w-1/4 overflow-y-auto" >
+const ListEps = ({name,episodes})=>(<div className="w-full md:w-1/4 overflow-y-auto" >
         {
             episodes.reverse().map((value)=>( <Link key={value?.number} to={`/infonew/${name}%${value?.number}`} className="block w-full cursor-pointer rounded-xl bg-teal-500 p-1 my-1 text-center font-semibold text-white transition hover:scale-105 hover:bg-teal-600">{`Episode - ${value?.number}`}</Link>))
         }
@@ -51,7 +51,7 @@ const InfoNew = () => {
                         <div className="space-y-8 rounded-xl bg-white p-5 my-2">
                             <div>{`${info?.title} - Eps ${currentEps}`}</div>
                             <div className="flex space-x-4 h-[36rem]">
-                                <div id="video-player" className="w-3/4 flex aspect-w-16">
+                                <div id="video-player" className="w-full md:w-3/4 aspect-w-16">
                                     <iframe
                                     className="m-x-auto overflow-hidden h-[32rem]"
                                     title="video player"
